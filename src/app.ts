6bd7edec17420d33465b03d8a885d5bcfa65d7a6
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-12-07 11:04:52 +0000

    renative to grimoire

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.get("/", (c) => {
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>jsdoc</title>
+    <title>Grimoire</title>
     <style>
       body { font-family: system-ui, -apple-system, sans-serif; margin: 2rem; }
       form { display: flex; gap: .5rem; flex-wrap: wrap; }
